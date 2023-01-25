92dee8b2ff9341f346612c5c2fb78f0834cfe6de
JIIN <j00j12i11@duksung.ac.kr>
2023-01-26 01:08:17 +0900

    0125

---
+++ app.js
@@ -0,0 +1,21 @@
+const express = require("express");
+const app = express();
+
+require("dotenv").config();
+
+const gamesRouter = require("./routes/games.router");
+const usersRouter = require("./routes/users.router");
+const scoresRouter = require("./routes/scores.router");
+
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
+
+app.use("/api/v1/games", gamesRouter);
+app.use("/api/v1/users", usersRouter);
+app.use("/api/v1/scores", scoresRouter);
+
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+  console.log("Server running...");
+});
