b43d7139defe83ff1a51bf2fea085fc1da22f611
JIIN <j00j12i11@duksung.ac.kr>
2023-01-28 19:07:02 +0900

    edit mysql -> rds

--- database/index.js
+++ database/index.js
@@ -1,11 +1,11 @@
 const mysql = require('mysql2');
 
 const pool = mysql.createPool({
-    host: process.env.DB_HOST,
-    port: process.env.DB_PORT,
-    user: process.env.DB_USERNAME,
-    password: process.env.DB_PASSWORD,
-    database: process.env.DB_NAME,
+    host: process.env.RDS_HOSTNAME,
+    port: process.env.RDS_PORT,
+    user: process.env.RDS_USERNAME,
+    password: process.env.RDS_PASSWORD,
+    database: process.env.RDS_DB_NAME,
   });
 
 module.exports = pool.promise();
